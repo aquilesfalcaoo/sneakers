97138e13d5642c76f19b1a3cbdbfb0d391f45fd6
Aquiles <aquilesfalcaoo@gmail.com>
2022-12-22 22:26:37 -0300

    feat: finalizado quantidade do produto

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,30 @@
 const cart = document.querySelector(".cart");
+const items_product = document.querySelectorAll(".item-colections");
+const imagem_principal = document.querySelector("#main_image");
+const quantity = document.querySelector(".quantity");
+
+let inputValue = 0;
 
 function showCart() {
   document.querySelector(".cart-content").classList.toggle("show");
 }
 
+function increaseQuantity() {
+  inputValue++;
+  quantity.value = inputValue;
+}
+
+function decreaseQuantity() {
+  inputValue--;
+  quantity.value = inputValue;
+  if (inputValue < 0) {
+    alert("Não é possível a quantidade ser menor que 0");
+    document.querySelector(".quantity").value = 0;
+  }
+}
+
 cart.addEventListener("click", showCart);
 
-const items_product = document.querySelectorAll(".item-colections");
-const imagem_principal = document.querySelector("#main_image");
 items_product.forEach((el) => {
   el.addEventListener("click", () => {
     for (let i = 0; i < items_product.length; i++) {
@@ -15,7 +32,6 @@ items_product.forEach((el) => {
         items_product[i].classList.remove("active");
         el.classList.add("active");
       }
-
       if (items_product[0].classList.contains("active")) {
         imagem_principal.src = "./assets/images/image-product-1.jpg";
       } else if (items_product[1].classList.contains("active")) {

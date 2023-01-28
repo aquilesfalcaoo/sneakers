30578ae67c586a219e67b30fbab702bc22a57793
Aquiles <aquilesfalcaoo@gmail.com>
2023-01-28 10:29:33 -0300

    feat: finalizando lógica de adicionar ao carrinho

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,6 +12,12 @@ cart.addEventListener("click", showCart);
 
 const items_product = document.querySelectorAll(".item-colections");
 const imagem_principal = document.querySelector("#main_image");
+const images = {
+  image_1: "./assets/images/image-product-1.jpg",
+  image_2: "./assets/images/image-product-2.jpg",
+  image_3: "./assets/images/image-product-3.jpg",
+  image_4: "./assets/images/image-product-4.jpg"
+};
 
 items_product.forEach((el) => {
   el.addEventListener("click", () => {
@@ -21,13 +27,13 @@ items_product.forEach((el) => {
         el.classList.add("active");
       }
       if (items_product[0].classList.contains("active")) {
-        imagem_principal.src = "./assets/images/image-product-1.jpg";
+        imagem_principal.src = images.image_1;
       } else if (items_product[1].classList.contains("active")) {
-        imagem_principal.src = "./assets/images/image-product-2.jpg";
+        imagem_principal.src = images.image_2;
       } else if (items_product[2].classList.contains("active")) {
-        imagem_principal.src = "./assets/images/image-product-3.jpg";
+        imagem_principal.src = images.image_3;
       } else {
-        imagem_principal.src = "./assets/images/image-product-4.jpg";
+        imagem_principal.src = images.image_4;
       }
     }
   });
@@ -51,5 +57,51 @@ button_for_minus.addEventListener("click", () => {
   if (count_quantity > 0) {
     count_quantity--;
     quantity.innerHTML = count_quantity;
+  }
+});
+
+//* Add a cart
+
+const addToCart = document.querySelector(".addCart");
+const countProduct = document.querySelector(".count-product");
+const cartEmptyMessage = document.querySelector(".cart-text");
+const cartProduct = document.querySelector(".cart-product");
+const productDelete = document.querySelector("#delete");
+
+let incrementProduct = 0;
+
+function addProduct() {
+  countProduct.style.display = "block";
+  incrementProduct++;
+  countProduct.innerHTML = incrementProduct;
+}
+
+function emptyCart() {
+  cartEmptyMessage.style.display = "none";
+}
+
+function showProduct() {
+  cartProduct.style.display = "block";
+}
+
+function deleteProduct() {
+  cartProduct.style.display = "none";
+  emptyCart();
+}
+
+productDelete.addEventListener('click', () => {
+  deleteProduct();
+  cartEmptyMessage.style.display = "block";
+  incrementProduct = 0;
+  countProduct.innerHTML = incrementProduct;
+});
+
+addToCart.addEventListener('click', () => {
+  if(count_quantity == 0) {
+    alert('A quantidade do produto não pode ser 0.');
+  } else {
+    addProduct();
+    emptyCart();
+    showProduct();
   }
 });
